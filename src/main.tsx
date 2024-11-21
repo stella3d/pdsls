1e700a9cce43f7aed0ea6f1eba3d6ee740232c56
notjuliet <notjuliet@riseup.net>
2024-11-21 04:05:57 +0100

    add record editing

--- src/main.tsx
+++ src/main.tsx
@@ -126,13 +126,20 @@ const RecordView: Component = () => {
   const params = useParams();
   const [record, setRecord] = createSignal<ComAtprotoRepoGetRecord.Output>();
   const [modal, setModal] = createSignal<HTMLDialogElement>();
-  const [open, setOpen] = createSignal(false);
+  const [openDelete, setOpenDelete] = createSignal(false);
+  const [openEdit, setOpenEdit] = createSignal(false);
 
   let clickEvent = (event: MouseEvent) => {
-    if (modal() && event.target == modal()) setOpen(false);
+    if (modal() && event.target == modal()) {
+      setOpenDelete(false);
+      setOpenEdit(false);
+    }
   };
   let keyEvent = (event: KeyboardEvent) => {
-    if (modal() && event.key == "Escape") setOpen(false);
+    if (modal() && event.key == "Escape") {
+      setOpenDelete(false);
+      setOpenEdit(false);
+    }
   };
 
   onMount(async () => {
@@ -176,6 +183,22 @@ const RecordView: Component = () => {
     "getRecord",
   );
 
+  const editRecord = action(async (formData: FormData) => {
+    const record = formData.get("record");
+    if (!record) return;
+    rpc = new XRPC({ handler: agent });
+    await rpc.call("com.atproto.repo.putRecord", {
+      data: {
+        repo: params.repo,
+        collection: params.collection,
+        rkey: params.rkey,
+        record: JSON.parse(record.toString()),
+      },
+    });
+    setOpenEdit(false);
+    setTimeout(async () => window.location.reload(), 500);
+  });
+
   const deleteRecord = action(async () => {
     rpc = new XRPC({ handler: agent });
     rpc.call("com.atproto.repo.deleteRecord", {
@@ -189,15 +212,57 @@ const RecordView: Component = () => {
   });
 
   createEffect(() => {
-    if (open()) document.body.style.overflow = "hidden";
+    if (openDelete() || openEdit()) document.body.style.overflow = "hidden";
     else document.body.style.overflow = "auto";
   });
 
   return (
     <Show when={record()}>
       <Show when={loginState() && agent.sub === params.repo}>
-        <div class="flex w-full justify-center">
-          <Show when={open()}>
+        <div class="flex w-full justify-center gap-x-2">
+          <Show when={openEdit()}>
+            <dialog
+              ref={setModal}
+              class="fixed left-0 top-0 z-[2] flex h-screen w-screen items-center justify-center bg-transparent font-sans"
+            >
+              <div class="dark:bg-dark-400 rounded-md border border-slate-900 bg-slate-100 p-4 text-slate-900 dark:border-slate-100 dark:text-slate-100">
+                <h3 class="mb-2 text-lg font-bold">Editing record</h3>
+                <form action={editRecord} method="post">
+                  <div class="size-fit">
+                    <textarea
+                      name="record"
+                      required
+                      class="dark:bg-dark-200 w-xs h-sm sm:w-xl sm:h-lg text-nowrap px-2 py-1 font-mono"
+                      spellcheck={false}
+                    >
+                      {JSON.stringify(record()?.value, null, 2)}
+                    </textarea>
+                  </div>
+                  <div class="mt-2 inline-flex w-full justify-end gap-2">
+                    <button
+                      onclick={() => setOpenEdit(false)}
+                      class="dark:bg-dark-900 dark:hover:bg-dark-800 rounded-lg bg-white px-2.5 py-1.5 text-sm font-bold hover:bg-slate-200 focus:outline-none focus:ring-2 focus:ring-slate-700 dark:focus:ring-slate-300"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      class="rounded-lg bg-green-500 px-2.5 py-1.5 text-sm font-bold text-slate-100 hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-slate-700 dark:bg-green-600 dark:hover:bg-green-500 dark:focus:ring-slate-300"
+                    >
+                      Confirm
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </dialog>
+          </Show>
+          <button
+            onclick={() => setOpenEdit(true)}
+            class="dark:bg-dark-700 dark:hover:bg-dark-800 rounded-lg border border-slate-400 bg-white px-2.5 py-1.5 font-sans text-sm font-bold hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-700 dark:focus:ring-slate-300"
+          >
+            Edit
+          </button>
+          <Show when={openDelete()}>
             <dialog
               ref={setModal}
               class="fixed left-0 top-0 z-[2] flex h-screen w-screen items-center justify-center bg-transparent font-sans"
@@ -207,7 +272,7 @@ const RecordView: Component = () => {
                 <form action={deleteRecord} method="post">
                   <div class="mt-2 inline-flex gap-2">
                     <button
-                      onclick={() => setOpen(false)}
+                      onclick={() => setOpenDelete(false)}
                       class="dark:bg-dark-900 dark:hover:bg-dark-800 rounded-lg bg-white px-2.5 py-1.5 text-sm font-bold hover:bg-slate-200 focus:outline-none focus:ring-2 focus:ring-slate-700 dark:focus:ring-slate-300"
                     >
                       Cancel
@@ -224,7 +289,7 @@ const RecordView: Component = () => {
             </dialog>
           </Show>
           <button
-            onclick={() => setOpen(true)}
+            onclick={() => setOpenDelete(true)}
             class="rounded-lg bg-red-500 px-2.5 py-1.5 font-sans text-sm font-bold text-slate-100 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-slate-700 dark:bg-red-600 dark:hover:bg-red-500 dark:focus:ring-slate-300"
           >
             Delete
