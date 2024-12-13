39d0f15b7250174d92d9b97c214af20a7a2b7418
notjuliet <notjuliet@riseup.net>
2024-12-13 17:53:29 +0100

    edit home page

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -3,37 +3,60 @@ import { Component } from "solid-js";
 const Home: Component = () => {
   return (
     <div class="mt-3 flex w-full flex-col break-words">
-      <div>
-        <span class="font-semibold text-orange-400">PDS URL</span> (https://
-        required):
-        <div>
-          <a href="/pds.bsky.mom" class="text-lightblue-500 hover:underline">
-            https://pds.bsky.mom
-          </a>
-        </div>
-      </div>
-      <div>
-        <span class="font-semibold text-orange-400">AT URI</span> (at://
-        optional, DID or handle alone also works):
-        <div>
+      <div class="mb-2">
+        <p>
+          Browse the public data on{" "}
           <a
-            href="/at/did:plc:oisofpd7lj26yvgiivf3lxsi/app.bsky.feed.post/3l2zpbbhuvw2h"
             class="text-lightblue-500 hover:underline"
+            href="https://atproto.com"
+            target="_blank"
           >
-            at://did:plc:oisofpd7lj26yvgiivf3lxsi/app.bsky.feed.post/3l2zpbbhuvw2h
+            AT Protocol
           </a>
-        </div>
+          .
+        </p>
+        <p>
+          You can optionally{" "}
+          <a href="/login" class="text-lightblue-500 hover:underline">
+            login
+          </a>{" "}
+          to manage the records on your repository.
+        </p>
       </div>
-      <div>
-        <span class="font-semibold text-orange-400">Bluesky Link</span> (posts
-        and profiles):
+      <p>The search input accepts:</p>
+      <div class="ml-2">
         <div>
-          <a
-            href="/at/did:plc:ia76kvnndjutgedggx2ibrem/app.bsky.feed.post/3kenlltlvus2u"
-            class="text-lightblue-500 hover:underline"
-          >
-            https://bsky.app/profile/mary.my.id/post/3kenlltlvus2u
-          </a>
+          <span class="font-semibold text-orange-400">PDS URL</span> (https://
+          required):
+          <div>
+            <a href="/pds.bsky.mom" class="text-lightblue-500 hover:underline">
+              https://pds.bsky.mom
+            </a>
+          </div>
+        </div>
+        <div>
+          <span class="font-semibold text-orange-400">AT URI</span> (at://
+          optional, DID or handle alone also works):
+          <div>
+            <a
+              href="/at/did:plc:oisofpd7lj26yvgiivf3lxsi/app.bsky.feed.post/3l2zpbbhuvw2h"
+              class="text-lightblue-500 hover:underline"
+            >
+              at://did:plc:oisofpd7lj26yvgiivf3lxsi/app.bsky.feed.post/3l2zpbbhuvw2h
+            </a>
+          </div>
+        </div>
+        <div>
+          <span class="font-semibold text-orange-400">Bluesky Link</span> (posts
+          and profiles):
+          <div>
+            <a
+              href="/at/did:plc:ia76kvnndjutgedggx2ibrem/app.bsky.feed.post/3kenlltlvus2u"
+              class="text-lightblue-500 hover:underline"
+            >
+              https://bsky.app/profile/mary.my.id/post/3kenlltlvus2u
+            </a>
+          </div>
         </div>
       </div>
     </div>
