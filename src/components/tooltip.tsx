05a7fb02c39f5f80d321ec6092f43393fb7d3f0f
notjuliet <notjuliet@riseup.net>
2025-01-04 11:01:50 +0100

    fix tooltip on mobile

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -5,17 +5,17 @@ const isTouchDevice = "ontouchstart" in window || navigator.maxTouchPoints > 1;
 const Tooltip = (props: { text: string; children: JSX.Element }) => {
   const width = props.text.length;
   return (
-    <Show when={!isTouchDevice}>
-      <div class="group/tooltip relative flex items-center font-sans">
-        {props.children}
+    <div class="group/tooltip relative flex items-center font-sans">
+      {props.children}
+      <Show when={!isTouchDevice}>
         <span
           style={`transform: translate(-50%, 2rem)`}
           class={`z-5 left-50% invisible absolute whitespace-nowrap dark:bg-neutral-800 min-w-[${width.toString()}ch] rounded border border-neutral-500 bg-white p-1 text-center text-xs group-hover/tooltip:visible`}
         >
           {props.text}
         </span>
-      </div>
-    </Show>
+      </Show>
+    </div>
   );
 };
 
