adc11d8dff10bf150598e494561ab28dde521e54
notjuliet <notjuliet@riseup.net>
2025-01-04 09:38:05 +0100

    remove tooltip on touchscreen

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,12 +1,18 @@
+import { Show } from "solid-js";
+
+const isTouchDevice = "ontouchstart" in window || navigator.maxTouchPoints > 1;
+
 const Tooltip = (props: { text: string }) => {
   const width = props.text.length;
   return (
-    <span
-      style={`transform: translate(-50%, 2rem)`}
-      class={`z-5 left-50% invisible absolute whitespace-nowrap dark:bg-neutral-800 min-w-[${width.toString()}ch] rounded border border-neutral-500 bg-white p-1 text-center text-xs group-hover/tooltip:visible`}
-    >
-      {props.text}
-    </span>
+    <Show when={!isTouchDevice}>
+      <span
+        style={`transform: translate(-50%, 2rem)`}
+        class={`z-5 left-50% invisible absolute whitespace-nowrap dark:bg-neutral-800 min-w-[${width.toString()}ch] rounded border border-neutral-500 bg-white p-1 text-center text-xs group-hover/tooltip:visible`}
+      >
+        {props.text}
+      </span>
+    </Show>
   );
 };
 
