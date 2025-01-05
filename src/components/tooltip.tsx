06a2e254080df7f5b2282c08008a896a63cf8cfd
notjuliet <notjuliet@riseup.net>
2025-01-05 23:36:05 +0100

    fix tooltip interaction

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -10,7 +10,7 @@ const Tooltip = (props: { text: string; children: JSX.Element }) => {
       <Show when={!isTouchDevice}>
         <span
           style={`transform: translate(-50%, 2rem)`}
-          class={`z-5 left-50% invisible absolute whitespace-nowrap dark:bg-neutral-800 min-w-[${width.toString()}ch] rounded border border-neutral-500 bg-white p-1 text-center text-xs group-hover/tooltip:visible`}
+          class={`z-5 left-50% pointer-events-none invisible absolute whitespace-nowrap dark:bg-neutral-800 min-w-[${width.toString()}ch] rounded border border-neutral-500 bg-white p-1 text-center text-xs group-hover/tooltip:visible`}
         >
           {props.text}
         </span>
