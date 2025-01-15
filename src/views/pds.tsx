b9d81e9e77abd177b95abb407a0c90f9a35a6809
notjuliet <notjuliet@riseup.net>
2025-01-15 20:50:20 +0100

    fix centering on pds view

--- src/views/pds.tsx
+++ src/views/pds.tsx
@@ -41,7 +41,7 @@ const PdsView = () => {
 
   return (
     <Show when={repos() || response()}>
-      <div class="mt-3 flex flex-col items-center">
+      <div class="mt-3 flex flex-col">
         <div class="flex max-w-[21rem] gap-1">
           <span class="font-semibold text-stone-600 dark:text-stone-400">
             Version
