26506ddb4298eb3861b6f64031f1513a5973c8a9
Juliet <m@juli.ee>
2025-02-27 16:16:28 -0800

    edit unocss config

--- uno.config.ts
+++ uno.config.ts
@@ -1,14 +1,3 @@
 import { defineConfig, presetWind3 } from "unocss";
 
-export default defineConfig({
-  presets: [presetWind3()],
-  extendTheme: (theme) => {
-    return {
-      ...theme,
-      breakpoints: {
-        ...theme.breakpoints,
-        xs: "400px",
-      },
-    };
-  },
-});
+export default defineConfig({ presets: [presetWind3()] });
