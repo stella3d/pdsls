7f319d245f674a40008c2782f2272db9f8a65a1d
Juliet <m@juli.ee>
2025-03-02 17:53:54 -0800

    polywasm fallback (#24)
    * test
    
    * test2
    
    * test3

--- src/views/record.tsx
+++ src/views/record.tsx
@@ -62,21 +62,19 @@ export default () => {
       setRecord(res.data);
       setCID(res.data.cid);
       setExternalLink(checkUri(res.data.uri));
-      if (wasmSupported) {
-        const publicTransport = await import("public-transport");
-        await publicTransport.authenticate_post_with_doc(
-          res.data.uri,
-          res.data.cid!,
-          res.data.value,
-          didDocCache[res.data.uri.split("/")[2]],
-        );
-        setValidRecord(true);
-      } else {
-        setNotice(
-          "Unable to validate record due to the browser not supporting WebAssembly.",
-        );
-        setValidRecord(false);
+      if (!wasmSupported) {
+        // @ts-ignore
+        const polywasm = await import("polywasm");
+        globalThis.WebAssembly = polywasm.WebAssembly;
       }
+      const publicTransport = await import("public-transport");
+      await publicTransport.authenticate_post_with_doc(
+        res.data.uri,
+        res.data.cid!,
+        res.data.value,
+        didDocCache[res.data.uri.split("/")[2]],
+      );
+      setValidRecord(true);
     } catch (err: any) {
       if (err.message) setNotice(err.message);
       else setNotice(`Invalid record: ${err}`);
