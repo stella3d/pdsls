8e3b23452c48c900febd8ec4b7ea9b2b46ef6db1
Mary <git@mary.my.id>
2025-03-15 12:51:47 +0700

    change stuff around

--- src/utils/types/at-uri.ts
+++ src/utils/types/at-uri.ts
@@ -1,16 +1,45 @@
+type Did<TMethod extends string = string> = `did:${TMethod}:${string}`;
+
+type Nsid = `${string}.${string}.${string}`;
+
+type RecordKey = string;
+
 const DID_RE = /^did:([a-z]+):([a-zA-Z0-9._:%\-]*[a-zA-Z0-9._\-])$/;
 
 const NSID_RE =
   /^[a-zA-Z](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+(?:\.[a-zA-Z](?:[a-zA-Z0-9]{0,62})?)$/;
 
-const RECORD_KEY_RE = /(?!\.{1,2}$)[a-zA-Z0-9_~.:-]{1,512}/;
+const RECORD_KEY_RE = /^(?!\.{1,2}$)[a-zA-Z0-9_~.:-]{1,512}$/;
 
 const ATURI_RE =
-  /^at:\/\/([a-zA-Z0-9._:%-]+)(?:\/([a-zA-Z0-9-.]+)(?:\/([a-zA-Z0-9._~:@!$&%')(*+,;=-]+)))(?:#(\/[a-zA-Z0-9._~:@!$&%')(*+,;=\-[\]/\\]*))?$/;
+  /^at:\/\/([a-zA-Z0-9._:%-]+)(?:\/([a-zA-Z0-9-.]+)(?:\/([a-zA-Z0-9._~:@!$&%')(*+,;=-]+))?)?(?:#(\/[a-zA-Z0-9._~:@!$&%')(*+,;=\-[\]/\\]*))?$/;
 
-type Did<TMethod extends string = string> = `did:${TMethod}:${string}`;
+const isDid = (input: unknown): input is Did => {
+  return (
+    typeof input === "string" &&
+    input.length >= 7 &&
+    input.length <= 2048 &&
+    DID_RE.test(input)
+  );
+};
 
-type Nsid = `${string}.${string}.${string}`;
+const isNsid = (input: unknown): input is Nsid => {
+  return (
+    typeof input === "string" &&
+    input.length >= 5 &&
+    input.length <= 317 &&
+    NSID_RE.test(input)
+  );
+};
+
+const isRecordKey = (input: unknown): input is RecordKey => {
+  return (
+    typeof input === "string" &&
+    input.length >= 1 &&
+    input.length <= 512 &&
+    RECORD_KEY_RE.test(input)
+  );
+};
 
 export interface AddressedAtUri {
   repo: Did;
@@ -19,32 +48,20 @@ export interface AddressedAtUri {
   fragment: string | undefined;
 }
 
-const isDid = (input: string): input is Did => {
-  return input.length >= 7 && input.length <= 2048 && DID_RE.test(input);
-};
-
-const isNsid = (str: string): str is Nsid => {
-  return str.length >= 5 && str.length <= 317 && NSID_RE.test(str);
-};
-
-const isRecordKey = (input: string) => {
-  return input.length >= 1 && input.length <= 512 && RECORD_KEY_RE.test(input);
-};
-
 export const parseAddressedAtUri = (str: string): AddressedAtUri => {
   const match = ATURI_RE.exec(str);
-  assert(match !== null, `invalid at-uri: ${str}`);
+  assert(match !== null, `invalid addressed-at-uri: ${str}`);
 
-  const [, repo, collection, rkey, fragment] = match;
-  assert(isDid(repo), `invalid repo in at-uri: ${repo}`);
-  assert(isNsid(collection), `invalid collection in at-uri: ${collection}`);
-  assert(isRecordKey(rkey), `invalid rkey in at-uri: ${rkey}`);
+  const [, r, c, k, f] = match;
+  assert(isDid(r), `invalid repo in addressed-at-uri: ${r}`);
+  assert(isNsid(c), `invalid collection in addressed-at-uri: ${c}`);
+  assert(isRecordKey(k), `invalid rkey in addressed-at-uri: ${k}`);
 
   return {
-    repo,
-    collection,
-    rkey,
-    fragment,
+    repo: r,
+    collection: c,
+    rkey: k,
+    fragment: f,
   };
 };
 
