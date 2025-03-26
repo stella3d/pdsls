7e9125a4bddee95feec242d04deb83ab5a9d5e1e
Juliet <m@juli.ee>
2025-03-25 19:51:08 -0700

    merge jetstream and firehose views

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,7 @@ import { RepoView } from "./views/repo.tsx";
 import { BlobView } from "./views/blob.tsx";
 import { CollectionView } from "./views/collection.tsx";
 import { LabelView } from "./views/labels.tsx";
-import { JetstreamView } from "./views/jetstream.tsx";
-import { SubscribeReposView } from "./views/subscribeRepos.tsx";
+import { StreamView } from "./views/stream.tsx";
 import { lazy } from "solid-js";
 
 const RecordView = lazy(() => import("./views/record.tsx"));
@@ -22,8 +21,8 @@ render(
   () => (
     <Router root={Layout}>
       <Route path="/" component={Home} />
-      <Route path="/jetstream" component={JetstreamView} />
-      <Route path="/firehose" component={SubscribeReposView} />
+      <Route path="/jetstream" component={StreamView} />
+      <Route path="/firehose" component={StreamView} />
       <Route path="/:pds" component={PdsView} />
       <Route path="/:pds/:repo" component={RepoView} />
       <Route path="/:pds/:repo/blobs" component={BlobView} />
--- src/views/stream.tsx
+++ src/views/stream.tsx
@@ -1,31 +1,48 @@
 import { createSignal, For, Show, onCleanup, onMount } from "solid-js";
 import { JSONValue } from "../components/json";
-import { action, useAction, useSearchParams } from "@solidjs/router";
+import { A, useLocation, useSearchParams } from "@solidjs/router";
+import { Firehose } from "../utils/firehose";
 
 const LIMIT = 25;
 type Parameter = { name: string; param: string | string[] | undefined };
+enum StreamType {
+  JETSTREAM,
+  FIREHOSE,
+}
 
-const JetstreamView = () => {
+const StreamView = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [parameters, setParameters] = createSignal<Parameter[]>([]);
+  const streamType =
+    useLocation().pathname === "/firehose" ?
+      StreamType.FIREHOSE
+    : StreamType.JETSTREAM;
 
   const [records, setRecords] = createSignal<Array<any>>([]);
   const [connected, setConnected] = createSignal(false);
   const [allEvents, setAllEvents] = createSignal(false);
   let socket: WebSocket;
+  let firehose: Firehose;
+  let formRef!: HTMLFormElement;
 
-  const connectSocket = action(async (formData: FormData) => {
+  const connectSocket = async (formData: FormData) => {
     if (connected()) {
-      socket?.close();
+      if (streamType === StreamType.JETSTREAM) socket?.close();
+      else firehose?.close();
       setConnected(false);
       return;
     }
     setRecords([]);
 
-    let url =
-      formData.get("instance")?.toString() ??
-      "wss://jetstream1.us-east.bsky.network/subscribe";
-    url = url.concat("?");
+    let url = "";
+    if (streamType === StreamType.JETSTREAM) {
+      url =
+        formData.get("instance")?.toString() ??
+        "wss://jetstream1.us-east.bsky.network/subscribe";
+      url = url.concat("?");
+    } else {
+      url = formData.get("instance")?.toString() ?? "wss://bsky.network";
+    }
 
     const collections = formData.get("collections")?.toString().split(",");
     collections?.forEach((collection) => {
@@ -39,8 +56,10 @@ const JetstreamView = () => {
     });
 
     const cursor = formData.get("cursor")?.toString();
-    if (cursor?.length) url = url.concat(`cursor=${cursor}`);
-    if (url.endsWith("&")) url = url.slice(0, -1);
+    if (streamType === StreamType.JETSTREAM) {
+      if (cursor?.length) url = url.concat(`cursor=${cursor}`);
+      if (url.endsWith("&")) url = url.slice(0, -1);
+    }
 
     if (searchParams.allEvents === "on") setAllEvents(true);
 
@@ -60,16 +79,46 @@ const JetstreamView = () => {
       { name: "All Events", param: formData.get("allEvents")?.toString() },
     ]);
 
-    socket = new WebSocket(url);
     setConnected(true);
-    socket.addEventListener("message", (event) => {
-      const rec = JSON.parse(event.data);
-      if (allEvents() || (rec.kind !== "account" && rec.kind !== "identity"))
-        setRecords(records().concat(rec).slice(-LIMIT));
-    });
-  });
-
-  const connect = useAction(connectSocket);
+    if (streamType === StreamType.JETSTREAM) {
+      socket = new WebSocket(url);
+      socket.addEventListener("message", (event) => {
+        const rec = JSON.parse(event.data);
+        if (allEvents() || (rec.kind !== "account" && rec.kind !== "identity"))
+          setRecords(records().concat(rec).slice(-LIMIT));
+      });
+    } else {
+      firehose = new Firehose({
+        relay: url,
+        cursor: cursor,
+      });
+      firehose.on("error", (err) => {
+        console.error(err);
+      });
+      firehose.on("commit", (commit) => {
+        for (const op of commit.ops) {
+          const record = {
+            $type: commit.$type,
+            repo: commit.repo,
+            seq: commit.seq,
+            time: commit.time,
+            rev: commit.rev,
+            since: commit.since,
+            tooBig: commit.tooBig,
+            op: op,
+          };
+          setRecords(records().concat(record).slice(-LIMIT));
+        }
+      });
+      firehose.on("identity", (identity) => {
+        setRecords(records().concat(identity).slice(-LIMIT));
+      });
+      firehose.on("account", (account) => {
+        setRecords(records().concat(account).slice(-LIMIT));
+      });
+      firehose.start();
+    }
+  };
 
   onMount(async () => {
     const formData = new FormData();
@@ -83,15 +132,30 @@ const JetstreamView = () => {
       formData.append("cursor", searchParams.cursor.toString());
     if (searchParams.allEvents)
       formData.append("allEvents", searchParams.allEvents.toString());
-    if (searchParams.instance) connect(formData);
+    if (searchParams.instance) connectSocket(formData);
   });
 
   onCleanup(() => socket?.close());
 
   return (
     <div class="mt-4 flex flex-col items-center gap-y-3">
-      <h1 class="text-lg font-bold">Jetstream Viewer</h1>
-      <form method="post" action={connectSocket} class="flex flex-col gap-y-3">
+      <div class="flex divide-x-2 text-lg font-bold">
+        <A
+          class="pr-2"
+          inactiveClass="text-lightblue-500 hover:underline"
+          href="/jetstream"
+        >
+          Jetstream
+        </A>
+        <A
+          class="pl-2"
+          inactiveClass="text-lightblue-500 hover:underline"
+          href="/firehose"
+        >
+          Firehose
+        </A>
+      </div>
+      <form ref={formRef} class="flex flex-col gap-y-3">
         <Show when={!connected()}>
           <label class="flex items-center justify-end gap-x-2">
             <span class="">Instance</span>
@@ -101,31 +165,37 @@ const JetstreamView = () => {
               spellcheck={false}
               value={
                 searchParams.instance ??
-                "wss://jetstream1.us-east.bsky.network/subscribe"
+                (streamType === StreamType.JETSTREAM ?
+                  "wss://jetstream1.us-east.bsky.network/subscribe"
+                : "wss://bsky.network")
               }
               class="w-16rem dark:bg-dark-100 rounded-lg border border-gray-400 px-2 py-1 focus:outline-none focus:ring-1 focus:ring-gray-300"
             />
           </label>
-          <label class="flex items-center justify-end gap-x-2">
-            <span class="">Collections</span>
-            <textarea
-              name="collections"
-              spellcheck={false}
-              placeholder="Comma-separated list of collections"
-              value={searchParams.collections ?? ""}
-              class="w-16rem dark:bg-dark-100 rounded-lg border border-gray-400 px-2 py-1 focus:outline-none focus:ring-1 focus:ring-gray-300"
-            />
-          </label>
-          <label class="flex items-center justify-end gap-x-2">
-            <span class="">DIDs</span>
-            <textarea
-              name="dids"
-              spellcheck={false}
-              placeholder="Comma-separated list of DIDs"
-              value={searchParams.dids ?? ""}
-              class="w-16rem dark:bg-dark-100 rounded-lg border border-gray-400 px-2 py-1 focus:outline-none focus:ring-1 focus:ring-gray-300"
-            />
-          </label>
+          <Show when={streamType === StreamType.JETSTREAM}>
+            <label class="flex items-center justify-end gap-x-2">
+              <span class="">Collections</span>
+              <textarea
+                name="collections"
+                spellcheck={false}
+                placeholder="Comma-separated list of collections"
+                value={searchParams.collections ?? ""}
+                class="w-16rem dark:bg-dark-100 rounded-lg border border-gray-400 px-2 py-1 focus:outline-none focus:ring-1 focus:ring-gray-300"
+              />
+            </label>
+          </Show>
+          <Show when={streamType === StreamType.JETSTREAM}>
+            <label class="flex items-center justify-end gap-x-2">
+              <span class="">DIDs</span>
+              <textarea
+                name="dids"
+                spellcheck={false}
+                placeholder="Comma-separated list of DIDs"
+                value={searchParams.dids ?? ""}
+                class="w-16rem dark:bg-dark-100 rounded-lg border border-gray-400 px-2 py-1 focus:outline-none focus:ring-1 focus:ring-gray-300"
+              />
+            </label>
+          </Show>
           <label class="flex items-center justify-end gap-x-2">
             <span class="">Cursor</span>
             <input
@@ -137,18 +207,20 @@ const JetstreamView = () => {
               class="w-16rem dark:bg-dark-100 rounded-lg border border-gray-400 px-2 py-1 focus:outline-none focus:ring-1 focus:ring-gray-300"
             />
           </label>
-          <div class="flex items-center justify-end gap-x-1">
-            <input
-              type="checkbox"
-              name="allEvents"
-              id="allEvents"
-              checked={searchParams.allEvents === "on" ? true : false}
-              onChange={(e) => setAllEvents(e.currentTarget.checked)}
-            />
-            <label for="allEvents" class="select-none">
-              Show account and identity events
-            </label>
-          </div>
+          <Show when={streamType === StreamType.JETSTREAM}>
+            <div class="flex items-center justify-end gap-x-1">
+              <input
+                type="checkbox"
+                name="allEvents"
+                id="allEvents"
+                checked={searchParams.allEvents === "on" ? true : false}
+                onChange={(e) => setAllEvents(e.currentTarget.checked)}
+              />
+              <label for="allEvents" class="select-none">
+                Show account and identity events
+              </label>
+            </div>
+          </Show>
         </Show>
         <Show when={connected()}>
           <div class="break-anywhere flex flex-col gap-1">
@@ -168,7 +240,8 @@ const JetstreamView = () => {
         </Show>
         <div class="flex justify-end">
           <button
-            type="submit"
+            type="button"
+            onclick={() => connectSocket(new FormData(formRef))}
             class="dark:bg-dark-700 dark:hover:bg-dark-800 w-fit rounded-lg border border-slate-400 bg-white px-2.5 py-1.5 text-sm font-bold hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-700 dark:focus:ring-slate-300"
           >
             {connected() ? "Disconnect" : "Connect"}
@@ -179,7 +252,7 @@ const JetstreamView = () => {
         <For each={records().toReversed()}>
           {(rec) => (
             <div class="pt-2">
-              <JSONValue data={rec} repo={rec.did} />
+              <JSONValue data={rec} repo={rec.did ?? rec.repo} />
             </div>
           )}
         </For>
@@ -188,4 +261,4 @@ const JetstreamView = () => {
   );
 };
 
-export { JetstreamView };
+export { StreamView };
