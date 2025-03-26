900f3887f8bfafeaa626d7c9bdd2a6f69099f997
Juliet <m@juli.ee>
2025-03-25 23:42:28 -0700

    fix create record submission

--- src/components/create.tsx
+++ src/components/create.tsx
@@ -4,7 +4,6 @@ import { agent } from "../components/login.jsx";
 import { Editor } from "../components/editor.jsx";
 import { editor } from "monaco-editor";
 import { theme } from "../components/settings.jsx";
-import { action, redirect } from "@solidjs/router";
 import { ComAtprotoRepoCreateRecord } from "@atcute/client/lexicons";
 import Tooltip from "./tooltip.jsx";
 
@@ -13,6 +12,8 @@ const CreateRecord = () => {
   const [openCreate, setOpenCreate] = createSignal(false);
   const [createNotice, setCreateNotice] = createSignal("");
   let model: editor.IModel;
+  let formRef!: HTMLFormElement;
+
   const placeholder = (date: string) => {
     return {
       $type: "app.bsky.feed.post",
@@ -38,7 +39,7 @@ const CreateRecord = () => {
 
   onCleanup(() => window.removeEventListener("keydown", keyEvent));
 
-  const createRecord = action(async (formData: FormData) => {
+  const createRecord = async (formData: FormData) => {
     const rpc = new XRPC({ handler: agent });
     const collection = formData.get("collection");
     const rkey = formData.get("rkey");
@@ -63,8 +64,8 @@ const CreateRecord = () => {
       return;
     }
     setOpenCreate(false);
-    throw redirect(`/${res.data.uri}`);
-  });
+    window.location.href = `/${res.data.uri}`;
+  };
 
   createEffect(() => {
     if (openCreate()) document.body.style.overflow = "hidden";
@@ -89,11 +90,7 @@ const CreateRecord = () => {
         >
           <div class="dark:bg-dark-400 rounded-md border border-slate-900 bg-slate-100 p-4 text-slate-900 dark:border-slate-100 dark:text-slate-100">
             <h3 class="mb-2 text-lg font-bold">Creating record</h3>
-            <form
-              class="flex flex-col gap-y-3"
-              action={createRecord}
-              method="post"
-            >
+            <form ref={formRef} class="flex flex-col gap-y-3">
               <div class="flex w-fit flex-col gap-y-3">
                 <div class="flex items-center gap-x-2">
                   <label for="collection" class="min-w-20 select-none">
@@ -151,7 +148,8 @@ const CreateRecord = () => {
                     Close
                   </button>
                   <button
-                    type="submit"
+                    type="button"
+                    onclick={() => createRecord(new FormData(formRef))}
                     class="rounded-lg bg-green-500 px-2.5 py-1.5 text-sm font-bold text-slate-100 hover:bg-green-400 focus:outline-none focus:ring-1 focus:ring-slate-700 dark:bg-green-600 dark:hover:bg-green-500 dark:focus:ring-slate-300"
                   >
                     Create
