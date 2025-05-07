416282090e9c6b01115fd200f00391c7bc382c45
Juliet <m@juli.ee>
2025-05-07 22:44:24 +0200

    deactivated/takendown repo view

--- src/views/repo.tsx
+++ src/views/repo.tsx
@@ -9,6 +9,7 @@ import Tooltip from "../components/tooltip.jsx";
 
 const RepoView = () => {
   const params = useParams();
+  const [error, setError] = createSignal<string>();
   const [downloading, setDownloading] = createSignal(false);
   const [didDoc, setDidDoc] = createSignal<DidDocument>();
   const [backlinks, setBacklinks] = createSignal<{
@@ -30,28 +31,41 @@ const RepoView = () => {
   const fetchRepo = async () => {
     if (!did.startsWith("did:")) did = await resolveHandle(params.repo);
     pds = await resolvePDS(did);
+    setDidDoc(didDocCache[did] as DidDocument);
+
     rpc = new Client({ handler: new CredentialManager({ service: pds }) });
     const res = await describeRepo(did);
-    if (!res.ok) throw new Error(res.data.error);
-    if (!res.data.collections) throw new Error(`Failed to fetch collections`);
-    const collections: Record<string, { hidden: boolean; nsids: string[] }> = {};
-    res.data.collections.forEach((c) => {
-      const nsid = c.split(".");
-      if (nsid.length > 2) {
-        const authority = `${nsid[0]}.${nsid[1]}`;
-        collections[authority] = {
-          nsids: (collections[authority]?.nsids ?? []).concat(nsid.slice(2).join(".")),
-          hidden: false,
-        };
+    if (res.ok) {
+      const collections: Record<string, { hidden: boolean; nsids: string[] }> = {};
+      res.data.collections.forEach((c) => {
+        const nsid = c.split(".");
+        if (nsid.length > 2) {
+          const authority = `${nsid[0]}.${nsid[1]}`;
+          collections[authority] = {
+            nsids: (collections[authority]?.nsids ?? []).concat(nsid.slice(2).join(".")),
+            hidden: false,
+          };
+        }
+      });
+      setNsids(collections);
+
+      // Initialize allCollapsed based on if all collections are hidden
+      setAllCollapsed(Object.keys(collections).every((authority) => collections[authority].hidden));
+    } else {
+      console.error(res.data.error);
+      switch (res.data.error) {
+        case "RepoDeactivated":
+          setError("This repository has been deactivated");
+          break;
+        case "RepoTakendown":
+          setError("This repository has been taken down");
+          break;
+        default:
+          setError("This repository is unreachable");
+          break;
       }
-    });
-    setNsids(collections);
+    }
 
-    // Initialize allCollapsed based on if all collections are hidden
-    const allHidden = Object.keys(collections).every((authority) => collections[authority].hidden);
-    setAllCollapsed(allHidden);
-
-    setDidDoc(didDocCache[did] as DidDocument);
     if (localStorage.backlinks === "true") {
       try {
         const backlinks = await getAllBacklinks(did);
@@ -111,59 +125,66 @@ const RepoView = () => {
   return (
     <Show when={repo()}>
       <div class="mt-3 flex w-[21rem] flex-col gap-2 break-words">
-        <div class="flex flex-col border-b border-neutral-500 pb-2 font-mono">
-          <div class="flex items-center gap-1">
-            <p class="font-sans font-semibold text-stone-600 dark:text-stone-400">Collections</p>
-            <Tooltip text={allCollapsed() ? "Expand all" : "Collapse all"}>
-              <button class="bg-transparent" onclick={toggleAllCollections}>
-                {allCollapsed() ?
-                  <div class="i-lucide-plus-copy text-xl" />
-                : <div class="i-lucide-minus-copy text-xl" />}
-              </button>
-            </Tooltip>
+        <Show when={error()}>
+          <div class="rounded-md bg-red-100 p-2 text-sm text-red-700 dark:bg-red-50 dark:text-red-600">
+            {error()}
           </div>
-          <div class="grid grid-cols-[min-content_1fr] items-center">
-            <For each={Object.keys(nsids() ?? {})}>
-              {(authority) => (
-                <>
-                  <Show when={nsids()?.[authority].hidden}>
+        </Show>
+        <Show when={nsids()}>
+          <div class="flex flex-col border-b border-neutral-500 pb-2 font-mono">
+            <div class="flex items-center gap-1">
+              <p class="font-sans font-semibold text-stone-600 dark:text-stone-400">Collections</p>
+              <Tooltip text={allCollapsed() ? "Expand all" : "Collapse all"}>
+                <button class="bg-transparent" onclick={toggleAllCollections}>
+                  {allCollapsed() ?
+                    <div class="i-lucide-plus-copy text-xl" />
+                  : <div class="i-lucide-minus-copy text-xl" />}
+                </button>
+              </Tooltip>
+            </div>
+            <div class="grid grid-cols-[min-content_1fr] items-center">
+              <For each={Object.keys(nsids() ?? {})}>
+                {(authority) => (
+                  <>
+                    <Show when={nsids()?.[authority].hidden}>
+                      <button
+                        class="i-lucide-plus-square mr-1"
+                        onclick={() => toggleCollection(authority)}
+                      />
+                    </Show>
+                    <Show when={!nsids()?.[authority].hidden}>
+                      <button
+                        class="i-lucide-minus-square mr-1"
+                        onclick={() => toggleCollection(authority)}
+                      />
+                    </Show>
                     <button
-                      class="i-lucide-plus-square mr-1"
+                      class="break-anywhere bg-transparent text-left"
                       onclick={() => toggleCollection(authority)}
-                    />
-                  </Show>
-                  <Show when={!nsids()?.[authority].hidden}>
-                    <button
-                      class="i-lucide-minus-square mr-1"
-                      onclick={() => toggleCollection(authority)}
-                    />
-                  </Show>
-                  <button
-                    class="break-anywhere bg-transparent text-left"
-                    onclick={() => toggleCollection(authority)}
-                  >
-                    {authority}
-                  </button>
-                  <Show when={!nsids()?.[authority].hidden}>
-                    <div></div>
-                    <div class="flex flex-col">
-                      <For each={nsids()?.[authority].nsids}>
-                        {(nsid) => (
-                          <A
-                            href={`/at://${did}/${authority}.${nsid}`}
-                            class="text-lightblue-500 break-anywhere hover:underline"
-                          >
-                            {authority}.{nsid}
-                          </A>
-                        )}
-                      </For>
-                    </div>
-                  </Show>
-                </>
-              )}
-            </For>
+                    >
+                      {authority}
+                    </button>
+                    <Show when={!nsids()?.[authority].hidden}>
+                      <div></div>
+                      <div class="flex flex-col">
+                        <For each={nsids()?.[authority].nsids}>
+                          {(nsid) => (
+                            <A
+                              href={`/at://${did}/${authority}.${nsid}`}
+                              class="text-lightblue-500 break-anywhere hover:underline"
+                            >
+                              {authority}.{nsid}
+                            </A>
+                          )}
+                        </For>
+                      </div>
+                    </Show>
+                  </>
+                )}
+              </For>
+            </div>
           </div>
-        </div>
+        </Show>
         <Show when={didDoc()}>
           {(didDocument) => (
             <div class="flex flex-col gap-y-1">
@@ -212,34 +233,36 @@ const RepoView = () => {
               <a
                 class="text-lightblue-500 flex w-fit items-center hover:underline"
                 href={
-                  repo()?.did.startsWith("did:plc") ?
-                    `https://plc.directory/${repo()?.did}`
-                  : `https://${repo()?.did.split("did:web:")[1]}/.well-known/did.json`
+                  did.startsWith("did:plc") ?
+                    `https://plc.directory/${did}`
+                  : `https://${did.split("did:web:")[1]}/.well-known/did.json`
                 }
                 target="_blank"
               >
                 DID document <div class="i-lucide-external-link ml-0.5 text-xs" />
               </a>
-              <Show when={repo()?.did.startsWith("did:plc")}>
+              <Show when={did.startsWith("did:plc")}>
                 <a
                   class="text-lightblue-500 flex w-fit items-center hover:underline"
-                  href={`https://boat.kelinci.net/plc-oplogs?q=${repo()?.did}`}
+                  href={`https://boat.kelinci.net/plc-oplogs?q=${did}`}
                   target="_blank"
                 >
                   PLC operation logs <div class="i-lucide-external-link ml-0.5 text-xs" />
                 </a>
               </Show>
-              <div class="flex items-center gap-1">
-                <button
-                  onclick={() => downloadRepo()}
-                  class="text-lightblue-500 flex w-fit items-center bg-transparent hover:underline"
-                >
-                  Export repo
-                </button>
-                <Show when={downloading()}>
-                  <div class="i-line-md-loading-twotone-loop" />
-                </Show>
-              </div>
+              <Show when={error()?.length === 0}>
+                <div class="flex items-center gap-1">
+                  <button
+                    onclick={() => downloadRepo()}
+                    class="text-lightblue-500 flex w-fit items-center bg-transparent hover:underline"
+                  >
+                    Export repo
+                  </button>
+                  <Show when={downloading()}>
+                    <div class="i-line-md-loading-twotone-loop" />
+                  </Show>
+                </div>
+              </Show>
               <Show when={backlinks()}>
                 {(backlinks) => (
                   <div class="mt-2 border-t border-neutral-500 pt-2">
