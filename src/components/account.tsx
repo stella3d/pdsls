154e486b312d4b6904bcb64330105781a1d7d894
Juliet <m@juli.ee>
2025-05-22 19:04:11 +0200

    account manager css tweaks

--- src/components/account.tsx
+++ src/components/account.tsx
@@ -84,7 +84,7 @@ const AccountManager = () => {
                   <div class="group/select flex w-full items-center justify-between gap-x-2">
                     <button
                       classList={{
-                        "bg-transparent basis-full text-left font-mono max-w-[32ch] text-sm truncate group-hover/select:bg-neutral-300 py-0.5 dark:group-hover/select:bg-neutral-600":
+                        "bg-transparent basis-full text-left font-mono max-w-[32ch] text-sm truncate group-hover/select:bg-gray-200 p-0.5 rounded dark:group-hover/select:bg-neutral-600":
                           true,
                         "text-green-500 dark:text-green-400": did === agent?.sub,
                       }}
