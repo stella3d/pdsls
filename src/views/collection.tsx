c2931cc1cf18c4cb4c66511189a549d009b5bdf4
Juliet <m@juli.ee>
2025-06-20 23:01:19 +0200

    tweak load more collection button

--- src/views/collection.tsx
+++ src/views/collection.tsx
@@ -193,7 +193,7 @@ const CollectionView = () => {
 
   return (
     <Show when={records.length || response()}>
-      <div class="z-5 dark:bg-dark-800 sticky top-0 mb-2 flex w-full flex-col items-center justify-center gap-2 border-b border-neutral-500 bg-zinc-100 py-4">
+      <div class="z-5 dark:bg-dark-800 sticky top-0 mb-2 flex w-full flex-col items-center justify-center gap-2 border-b border-neutral-500 bg-zinc-100 py-3">
         <div class="flex w-full items-center gap-2">
           <Show when={loginState() && agent.sub === did}>
             <div
@@ -305,13 +305,13 @@ const CollectionView = () => {
                 <button
                   type="button"
                   onclick={() => refetch()}
-                  class="dark:hover:bg-dark-300 rounded-lg border border-gray-400 bg-transparent px-2 py-1.5 text-sm font-bold hover:bg-zinc-50 focus:outline-none focus:ring-1 focus:ring-gray-300"
+                  class="dark:hover:bg-dark-300 rounded-lg border border-gray-400 bg-transparent px-2 py-1.5 text-xs font-bold hover:bg-zinc-50 focus:outline-none focus:ring-1 focus:ring-gray-300"
                 >
                   Load More
                 </button>
               </Show>
               <Show when={response.loading}>
-                <div class="i-eos-icons-loading text-2xl" />
+                <div class="i-eos-icons-loading text-xl" />
               </Show>
             </div>
           </Show>
